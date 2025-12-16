9478524955d76cc86ddeb2bb13a2f9b7f53a4206
Subash Singh Dhami <subashdhamee@gmail.com>
2025-12-16 20:41:01 +0545

    add initial project setup

---
+++ src/main/java/com/developer/pkg/PersonalKnowledgeGraphApplication.java
@@ -0,0 +1,13 @@
+package com.developer.pkg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalKnowledgeGraphApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalKnowledgeGraphApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/developer/pkg/PersonalKnowledgeGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developer.pkg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalKnowledgeGraphApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
