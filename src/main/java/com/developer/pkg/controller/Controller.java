4705bdcb75307f4b0a9368071a9e3ab1abb4f164
Subash Singh Dhami <subashdhamee@gmail.com>
2025-12-30 20:49:11 +0545

    add base page

---
+++ src/main/java/com/developer/pkg/controller/Controller.java
@@ -0,0 +1,12 @@
+package com.developer.pkg.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+}
